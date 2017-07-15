a39d71da59e6e7928288cea2c8a09217d13e9c98
Thomas Sjolshagen <thomas@eighty20results.com>
2017-07-15 11:37:21 -0400

    BUG FIX: Error/conflict when attempting to load new editor HTML/code ENHANCEMENT: Refactored for WordPress Codex improvements

--- class/editor/class.template-editor-view.php
+++ class/editor/class.template-editor-view.php
@@ -25,13 +25,13 @@ use E20R\Payment_Warning\Utilities\Utilities;
 class Template_Editor_View {
 	
 	public static function editor( $template_settings ) {
-	    
-	    if ( ! current_user_can( apply_filters( 'e20rpw_min_settings_capabilities', 'manage_options' ) ) ) {
-	        wp_die( __( "You do not have sufficient permissions to access this page", Payment_Warning::plugin_slug ) );
-        }
-        
-        $util = Utilities::get_instance();
-	    ?>
+		
+		if ( ! current_user_can( apply_filters( 'e20rpw_min_settings_capabilities', 'manage_options' ) ) ) {
+			wp_die( __( "You do not have sufficient permissions to access this page", Payment_Warning::plugin_slug ) );
+		}
+		
+		$util = Utilities::get_instance();
+		?>
         <h2><?php _e( 'Edit: Payment Warning Message Templates', Payment_Warning::plugin_slug ); ?></h2>
         <table class="form-table">
             <thead>
@@ -52,15 +52,15 @@ class Template_Editor_View {
                 <td>
                     <select name="e20r-message-templates" id="e20r-message-templates">
                         <option value="-1" selected="selected">
-                            <?php printf( '--- %s ---', __( 'Select a Message template to Edit', Payment_Warning::plugin_slug ) ); ?>
+							<?php printf( '--- %s ---', __( 'Select a Message template to Edit', Payment_Warning::plugin_slug ) ); ?>
                         </option>
-                        <?php foreach ( $template_settings as $template_name => $template ) { ?>
+						<?php foreach ( $template_settings as $template_name => $template ) { ?>
                             <option value="<?php esc_attr_e( $template_name ); ?>">
-                                <?php esc_html_e( $template['description'] ); ?>
+								<?php esc_html_e( $template['description'] ); ?>
                             </option>
-                        <?php } ?>
+						<?php } ?>
                         <option value="new">
-		                    <?php printf( '*** %s ***', __( 'Add new Message template', Payment_Warning::plugin_slug ) ); ?>
+							<?php printf( '*** %s ***', __( 'Add new Message template', Payment_Warning::plugin_slug ) ); ?>
                         </option>
                     </select>
                     <img src="<?php echo admin_url( 'images/wpspin_light.gif' ); ?>" id="e20r-busy-image"
@@ -73,12 +73,13 @@ class Template_Editor_View {
                 <td class="e20r-hidden-template-settings">
                     <table class="e20r-message-settings">
                         <tbody>
-                        <form action="" method="post" enctype="multipart/form-data" id="e20r-message-templates-form_<?php esc_attr_e( $template_name ); ?>">
-	                        <?php wp_nonce_field( Payment_Warning::plugin_prefix, 'message_template' ); ?>
-                        <?php
-                        foreach ( $template_settings as $template_name => $template ) {
-                            self::add_template_entry( $template_name, $template );
-                        } ?>
+                        <form action="" method="post" enctype="multipart/form-data"
+                              id="e20r-message-templates-form_<?php esc_attr_e( $template_name ); ?>">
+							<?php wp_nonce_field( Payment_Warning::plugin_prefix, 'message_template' ); ?>
+							<?php
+							foreach ( $template_settings as $template_name => $template ) {
+								self::add_template_entry( $template_name, $template );
+							} ?>
                         </form>
                         </tbody>
                     </table>
@@ -99,122 +100,136 @@ class Template_Editor_View {
                                value="<?php _e( 'Add New Template', Payment_Warning::plugin_slug ); ?>"/>
                         -->
                     </p>
-                    
+
                 </td>
             </tr>
             </tfoot>
         </table>
-        <?php
+		<?php
 	}
 	
 	public static function add_template_entry( $template_name, $template, $return = false ) {
-	    
-	    if ( true === $return ) {
-		    ob_start();
-	    }?>
-            <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
-                <input type="hidden" name="e20r_message_template-file_name" value="<?php esc_attr_e( $template['file_name'] ); ?>">
-                <input type="hidden" name="e20r_message_template-key" value="<?php esc_attr_e( $template_name ); ?>">
-                <input type="hidden" name="e20r_message_template-file_path" value="<?php esc_attr_e( $template['file_path'] ); ?>">
-                <input type="hidden" name="e20r_message_template-description" value="<?php esc_attr_e( $template['description'] ); ?>">
-                <td colspan="2"><h4 style="text-align: center;"><?php esc_html_e($template['description'] );?></h4></td>
-            </tr>
-            <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
-                <th scope="row" valign="top" class="e20r-message-template-col">
-                    <label for="e20r-message-type_<?php esc_attr_e( $template_name ); ?>">
-						<?php _e( 'Message type', Payment_Warning::plugin_slug ); ?>
-                    </label>
-                </th>
-                <td class="e20r-message-template-col">
-                    <select id="e20r-message-type_<?php esc_attr_e( $template_name ); ?>" name="e20r_message_template-type">
-                        <option value="-1" <?php selected( $template['type'], null ); ?>>
-							<?php _e( 'Header/Footer', Payment_Warning::plugin_slug ); ?>
-                        </option>
-                        <option value="expiration" <?php selected( $template['type'], 'expiration' ); ?>>
-							<?php _e( 'Warning', Payment_Warning::plugin_slug ); ?>
-                        </option>
-                        <option value="recurring" <?php selected( $template['type'], 'recurring' ); ?>>
-							<?php _e( 'Reminder', Payment_Warning::plugin_slug ); ?>
-                        </option>
-                    </select>
-                </td>
-            </tr>
-            <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?> e20r-message-schedule-info">
-                <th scope="row" valign="top" class="e20r-message-template-col">
-                    <label for="e20r-message-schedule_<?php esc_attr_e( $template_name ); ?>">
-						<?php _e( 'Send schedule', Payment_Warning::plugin_slug ); ?>
-                    </label>
-                </th>
-                <td class="e20r-message-template-col">
-					<?php
-					if ( ! empty( $template['schedule'] ) ) {
-						foreach ( $template['schedule'] as $days ) { ?>
-                            <div class="e20r-schedule-entry">
-                                <input name="e20r_message_template-schedule[]"
-                                       type="number"
-                                       value="<?php esc_attr_e( $days ); ?>"
-                                       class="e20r-message-schedule" />&nbsp;
-                                <span class="e20r-message-schedule-label">
+		
+		if ( true === $return ) {
+			ob_start();
+		} ?>
+        <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
+            <input type="hidden" name="e20r_message_template-file_name"
+                   value="<?php esc_attr_e( $template['file_name'] ); ?>">
+            <input type="hidden" name="e20r_message_template-key" value="<?php esc_attr_e( $template_name ); ?>">
+            <input type="hidden" name="e20r_message_template-file_path"
+                   value="<?php esc_attr_e( $template['file_path'] ); ?>">
+            <input type="hidden" name="e20r_message_template-description"
+                   value="<?php esc_attr_e( $template['description'] ); ?>">
+            <td colspan="2"><h4 style="text-align: center;"><?php esc_html_e( $template['description'] ); ?></h4></td>
+        </tr>
+        <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
+            <th scope="row" valign="top" class="e20r-message-template-col">
+                <label for="e20r-message-type_<?php esc_attr_e( $template_name ); ?>">
+					<?php _e( 'Message type', Payment_Warning::plugin_slug ); ?>
+                </label>
+            </th>
+            <td class="e20r-message-template-col">
+                <select id="e20r-message-type_<?php esc_attr_e( $template_name ); ?>" name="e20r_message_template-type">
+                    <option value="-1" <?php selected( $template['type'], null ); ?>>
+						<?php _e( 'Header/Footer', Payment_Warning::plugin_slug ); ?>
+                    </option>
+                    <option value="expiration" <?php selected( $template['type'], 'expiration' ); ?>>
+						<?php _e( 'Warning', Payment_Warning::plugin_slug ); ?>
+                    </option>
+                    <option value="recurring" <?php selected( $template['type'], 'recurring' ); ?>>
+						<?php _e( 'Reminder', Payment_Warning::plugin_slug ); ?>
+                    </option>
+                </select>
+            </td>
+        </tr>
+        <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?> e20r-message-schedule-info">
+            <th scope="row" valign="top" class="e20r-message-template-col">
+                <label for="e20r-message-schedule_<?php esc_attr_e( $template_name ); ?>">
+					<?php _e( 'Send schedule', Payment_Warning::plugin_slug ); ?>
+                </label>
+            </th>
+            <td class="e20r-message-template-col">
+				<?php
+				if ( ! empty( $template['schedule'] ) ) {
+					foreach ( $template['schedule'] as $days ) { ?>
+                        <div class="e20r-schedule-entry">
+                            <input name="e20r_message_template-schedule[]"
+                                   type="number"
+                                   value="<?php esc_attr_e( $days ); ?>"
+                                   class="e20r-message-schedule"/>&nbsp;
+                            <span class="e20r-message-schedule-label">
                                                         <?php _e( 'days before event', Payment_Warning::plugin_slug ); ?>
                                                     </span>
-                                <span class="e20r-message-schedule-remove">
-                                                        <input type="button" value="<?php _e( "Remove", Payment_Warning::plugin_slug ); ?>" class="e20r-delete-schedule-entry button-secondary" />
+                            <span class="e20r-message-schedule-remove">
+                                                        <input type="button"
+                                                               value="<?php _e( "Remove", Payment_Warning::plugin_slug ); ?>"
+                                                               class="e20r-delete-schedule-entry button-secondary"/>
                                                     </span>
-                            </div>
-						<?php }
-						?><button class="button-secondary e20r-add-new-schedule"><?php _e( "Add new schedule entry", Payment_Warning::plugin_slug ); ?></button><?php
+                        </div>
+					<?php }
+					?>
+                    <button class="button-secondary e20r-add-new-schedule"><?php _e( "Add new schedule entry", Payment_Warning::plugin_slug ); ?></button><?php
+				} else {
+					if ( in_array( $template_name, array( 'messagefooter', 'messageheader' ) ) ) {
+						_e( "No schedule needed/defined", Payment_Warning::plugin_slug );
+					}
+				} ?>
+            </td>
+        </tr>
+        <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
+            <th scope="row" valign="top" class="e20r-message-template-col">
+                <label for="e20r-message-template-subject_<?php esc_attr_e( $template_name ); ?>">
+					<?php _e( 'Message subject', Payment_Warning::plugin_slug ); ?>
+                </label>
+            </th>
+            <td class="e20r-message-template-col">
+                <input id="e20r-message-template-subject_<?php esc_attr_e( $template_name ); ?>"
+                       name="e20r_message_template-subject"
+                       type="text" size="100" value="<?php echo esc_html( $template['subject'] ); ?>"/>
+            </td>
+        </tr>
+        <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
+            <th scope="row" valign="top" class="e20r-message-template-col">
+                <label for="e20r-message-template-body_<?php esc_attr_e( $template_name ); ?>">
+					<?php _e( 'Body', Payment_Warning::plugin_slug ); ?>
+                </label>
+            </th>
+            <td class="e20r-message-template-col">
+                <div class="template_editor_container">
+					<?php
+					if ( false === $return && 'new' !== $template_name ) {
+						wp_editor( ( ! empty( $template['body'] ) ? $template['body'] : null ), "e20r-message-body_{$template_name}", array( 'editor_height' => 350 ) );
 					} else {
-						if ( in_array( $template_name,array( 'messagefooter', 'messageheader' ) ) ) {
-							_e( "No schedule needed/defined", Payment_Warning::plugin_slug );
-						}
-					}?>
-                </td>
-            </tr>
-            <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
-                <th scope="row" valign="top" class="e20r-message-template-col">
-                    <label for="e20r-message-template-subject_<?php esc_attr_e( $template_name ); ?>">
-						<?php _e( 'Message subject', Payment_Warning::plugin_slug ); ?>
-                    </label>
-                </th>
-                <td class="e20r-message-template-col">
-                    <input id="e20r-message-template-subject_<?php esc_attr_e( $template_name ); ?>"
-                           name="e20r_message_template-subject"
-                           type="text" size="100" value="<?php echo esc_html( $template['subject'] ); ?>"/>
-                </td>
-            </tr>
-            <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
-                <th scope="row" valign="top" class="e20r-message-template-col">
-                    <label for="e20r-message-template-body_<?php esc_attr_e( $template_name ); ?>">
-						<?php _e( 'Body', Payment_Warning::plugin_slug ); ?>
-                    </label>
-                </th>
-                <td class="e20r-message-template-col">
-                    <div class="template_editor_container">
-                        <?php
-                        wp_editor( (! empty( $template['body'] ) ? $template['body'] : null ),"e20r-message-body_{$template_name}", array( 'editor_height' => 350 ) ); ?>
-                    </div>
-                </td>
-            </tr>
-            <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
-                <th class="e20r-message-template-col"></th>
-                <td class="e20r-message-template-col">
-                    <input id="e20r-message-disabled_<?php esc_attr_e( $template_name ); ?>"
-                           name="e20r_message_template-active" type="checkbox" value="1" <?php checked( $template['active'], 0 ); ?> />
-                    <label>
-                        <span id="e20r-message-disabled-label"><?php _e( 'Disable this template?', Payment_Warning::plugin_slug ); ?></span>
-                    </label>
-                    <p class="description small">
-						<?php _e( 'This template will not be used or sent.', Payment_Warning::plugin_slug ); ?>
-                    </p>
-                </td>
-            </tr>
-            <tr class="e20r-message-template-name_<?php esc_attr_e( $template_name ); ?> e20r-start-hidden">
-                <td colspan="2" class="e20r-message-template-col"><hr /></td>
-            </tr>
-        <?php
-        
-        if ( true === $return ) {
-	        return ob_get_clean();
-        }
-    }
+						?>
+                        <textarea name="e20r-message-body_new" id="e20r-message-body_new"></textarea><?php
+					} ?>
+                </div>
+            </td>
+        </tr>
+        <tr class="e20r-start-hidden e20r-message-template-name_<?php esc_attr_e( $template_name ); ?>">
+            <th class="e20r-message-template-col"></th>
+            <td class="e20r-message-template-col">
+                <input id="e20r-message-disabled_<?php esc_attr_e( $template_name ); ?>"
+                       name="e20r_message_template-active" type="checkbox"
+                       value="1" <?php checked( $template['active'], 0 ); ?> />
+                <label>
+                    <span id="e20r-message-disabled-label"><?php _e( 'Disable this template?', Payment_Warning::plugin_slug ); ?></span>
+                </label>
+                <p class="description small">
+					<?php _e( 'This template will not be used or sent.', Payment_Warning::plugin_slug ); ?>
+                </p>
+            </td>
+        </tr>
+        <tr class="e20r-message-template-name_<?php esc_attr_e( $template_name ); ?> e20r-start-hidden">
+            <td colspan="2" class="e20r-message-template-col">
+                <hr/>
+            </td>
+        </tr>
+		<?php
+		
+		if ( true === $return ) {
+			return ob_get_clean();
+		}
+	}
 }
