4969e765cf5b595b817c4fdd53abef943f1bb93d
Thomas Sjolshagen <thomas@eighty20results.com>
2017-07-22 17:15:44 -0400

    ENHANCEMENT: Refactor for better WordPress style compliance ENHANCEMENT: Add email notice to admin about completion of updates/notices being sent

--- class/class.handle-messages.php
+++ class/class.handle-messages.php
@@ -36,15 +36,15 @@ class Handle_Messages extends E20R_Background_Process {
 	public function __construct( $calling_class ) {
 		
 		$util = Utilities::get_instance();
-		$util->log("Instantiated Handle_Messages class");
+		$util->log( "Instantiated Handle_Messages class" );
 		
 		self::$instance = $this;
 		
-		$av = get_class( $calling_class );
-		$name = explode( '\\', $av );
-		$this->action = "hm_" . strtolower( $name[(count( $name ) - 1 )] );
+		$av           = get_class( $calling_class );
+		$name         = explode( '\\', $av );
+		$this->action = "hm_" . strtolower( $name[ ( count( $name ) - 1 ) ] );
 		
-		$util->log("Set Action variable to {$this->action} for Handle_Messages");
+		$util->log( "Set Action variable to {$this->action} for Handle_Messages" );
 		
 		// Required: Run the parent class constructor
 		parent::__construct();
@@ -61,35 +61,36 @@ class Handle_Messages extends E20R_Background_Process {
 		
 		$util = Utilities::get_instance();
 		
-		$util->log("Trigger per user message operation for " . $message->get_user()->get_user_ID() );
+		$util->log( "Trigger per user message operation for " . $message->get_user()->get_user_ID() );
 		
 		$template_type = $message->get_template_type();
-		$schedule = $message->get_schedule();
+		$schedule      = $message->get_schedule();
 		
 		if ( empty( $template_type ) ) {
-			$util->log(sprintf( "Unable to process %s Message based on %s for ", $message->get_template_type(), $message->get_user()->get_user_ID() ) );
+			$util->log( sprintf( "Unable to process %s Message based on %s for ", $message->get_template_type(), $message->get_user()->get_user_ID() ) );
+			
 			return false;
 		}
 		// $util->log( "Sending schedule for {$template_type} is: " . print_r( $schedule, true ) );
 		
-		foreach( $schedule as $interval_day ) {
+		foreach ( $schedule as $interval_day ) {
 			
 			$user_id = $message->get_user()->get_user_ID();
-			$send = false;
-			$util->log("Processing for warning day #{$interval_day}, user {$user_id}" );
+			$send    = false;
+			$util->log( "Processing for warning day #{$interval_day}, user {$user_id}" );
 			
 			$next_payment = $message->get_user()->get_next_payment();
-			$util->log("Found Next payment info: {$next_payment} for {$user_id}" );
+			$util->log( "Found Next payment info: {$next_payment} for {$user_id}" );
 			
-			if ( !empty( $next_payment ) ) {
+			if ( ! empty( $next_payment ) ) {
 				
 				$send = $message->should_send( $next_payment, $interval_day, $template_type );
 			}
 			
-			$util->log("Should we send {$user_id} the {$template_type} email message? " . ( $send ? 'Yes' : 'No') );
+			$util->log( "Should we send {$user_id} the {$template_type} email message? " . ( $send ? 'Yes' : 'No' ) );
 			
 			if ( true === $send ) {
-				$util->log("Preparing the message to {$user_id}");
+				$util->log( "Preparing the message to {$user_id}" );
 				$message->send_message( $template_type );
 			}
 		}
@@ -108,8 +109,96 @@ class Handle_Messages extends E20R_Background_Process {
 		
 		parent::complete();
 		// Show notice to user or perform some other arbitrary task...
+		
+		$this->send_admin_notice( 'recurring' );
+		$this->send_admin_notice( 'expiration' );
+		$this->send_admin_notice( 'creditcard' );
+		
 		$util = Utilities::get_instance();
-		$util->log("Completed message transmission operations");
+		$util->log( "Completed message transmission operations" );
 		// $util->add_message( __("Fetched user subscription data for all active gateway add-ons", Payment_Warning::plugin_slug ), 'info', 'backend' );
+	}
+	
+	/**
+	 * Send email message notifying administrator user(s) of completion
+	 *
+	 * @param $type
+	 *
+	 * @return bool
+	 *
+	 * @access private
+	 */
+	private function send_admin_notice( $type ) {
+		
+		$users     = array();
+		$today     = date_i18n( 'Y-m-d', current_time( 'timestamp' ) );
+		$type_text = null;
+		$subject   = sprintf( __( "Completed processing the %s payment warning type", Payment_Warning::plugin_slug ), $type );
+		
+		switch ( $type ) {
+			case 'recurring':
+				$users     = get_option( 'e20r_pw_sent_recurring', array() );
+				$type_text = __( "upcoming recurring payment", Payment_Warning::plugin_slug );
+				$subject   = sprintf( __( "Recurring Payment Warning sent to %d users on %s", Payment_Warning::plugin_slug ), count( $users ), $today );
+				
+				break;
+			
+			case 'expiration':
+				$users     = get_option( 'e20r_pw_sent_expiration', array() );
+				$type_text = __( "pending membership expiration", Payment_Warning::plugin_slug );
+				$subject   = sprintf( __( "Expiration Warning sent to %d users on %s", Payment_Warning::plugin_slug ), count( $users ), $today );
+				
+				break;
+			
+			case 'creditcard':
+				$users     = get_option( 'e20r_pw_sent_creditcard', array() );
+				$type_text = __( "credit card expiration", Payment_Warning::plugin_slug );
+				$subject   = sprintf( __( "Credit Card Expiration Warning sent to %d users on %s", Payment_Warning::plugin_slug ), count( $users ), $today );
+				
+				break;
+			
+		}
+		
+		$admin_intro_text = apply_filters(
+			"e20r-payment-warning-email-pre-list-text",
+			sprintf(
+				__( "A %s warning message has been sent to the following list of users/members:", Payment_Warning::plugin_slug ),
+				$type_text
+			)
+		);
+		
+		$body      = sprintf( "<div>%s</div>", $admin_intro_text );
+		$user_list = '<div style="font-size: 11pt; font-style: italic;">';
+		
+		if ( ! empty( $users[ $today ] ) ) {
+			
+			$user_list .= implode( '<br/>', $users );
+			
+		} else {
+			$user_list .= sprintf( __( "No %s warning emails sent/recorded for %s", Payment_Warning::plugin_slug ), $type_text, $today );
+		}
+		
+		$user_list .= '</div>';
+		$body      .= $user_list;
+		
+		$admin_address = get_option( 'admin_email' );
+		$headers       = array();
+		$cc_list       = apply_filters( 'e20r_payment_warning_admin_cc_list', array() );
+		$bcc_list      = apply_filters( 'e20r_payment_warning_admin_bcc_list', array() );
+		
+		if ( ! empty( $cc_list ) ) {
+			
+			foreach ( $cc_list as $cc_to ) {
+				$headers[] = "Cc: {$cc_to}";
+			}
+		}
+		
+		if ( ! empty( $bcc_list ) ) {
+			foreach ( $bcc_list as $bcc_to ) {
+				$headers[] = "Bcc: {$bcc_to}";
+			}
+		}
+		
+		return wp_mail( $admin_address, $subject, $body, $headers );
 	}
 }
