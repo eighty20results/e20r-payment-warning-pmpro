02f7d3c3ebb25ac47cb0adfec17cfc016b5ad8f7
Thomas Sjolshagen <thomas@eighty20results.com>
2017-12-31 12:03:48 -0700

    BUG FIX: Only load admin version of script if it exists in the file system

--- class/editor/class.editor.php
+++ class/editor/class.editor.php
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @version 1.5
+ * @version 1.6
  */
 
 namespace E20R\Utilities\Editor;
