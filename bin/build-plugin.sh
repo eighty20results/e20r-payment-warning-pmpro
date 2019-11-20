12bd6242ae1999ef773976e18390f0469fcb8bed
Thomas Sjolshagen <thomas@eighty20results.com>
2019-11-20 11:41:44 -0700

    ENHANCEMENT: Build json file containing list of supported API versions ENHANCEMENT: Generate and load supported Stripe API versions to upstream server

--- bin/build-plugin.sh
+++ bin/build-plugin.sh
@@ -4,7 +4,7 @@
 #
 short_name="e20r-payment-warning-pmpro"
 server="eighty20results.com"
-include=(class css javascript libraries languages LICENSE plugin-updates templates class.${short_name}.php README.md readme.txt)
+include=(class css javascript libraries languages LICENSE templates class.${short_name}.php README.md readme.txt)
 exclude=(*.yml *.phar composer.* vendor)
 build=(plugin-updates/vendor/*.php)
 plugin_path="${short_name}"
@@ -14,10 +14,17 @@ src_path="../"
 dst_path="../build/${plugin_path}"
 kit_path="../build/kits"
 kit_name="${kit_path}/${short_name}-${version}"
+api_path="./api_files"
+php_tool=/usr/bin/php
 debug_name="${kit_path}-debug/${short_name}-debug-${version}"
 debug_path="../build/${plugin_path}-debug"
 
 echo "Building kit for version ${version}"
+echo "Prepare all API files for version ${version} of the plugin"
+
+for entry in ${api_path}/*.txt; do
+	exec ${php_tool} ./convert-to-json.php ${entry}
+done
 
 rm ${src_path}logs/paypal.log
 
@@ -51,10 +58,15 @@ for e in ${exclude[@]}; do
     find ${debug_path} -type d -iname ${e} -exec rm -rf {} \;
 done
 
-mkdir -p ${dst_path}/plugin-updates/vendor/
-for b in ${build[@]}; do
-    cp ${src_path}${b} ${dst_path}/plugin-updates/vendor/
-    cp ${src_path}${b} ${debug_path}/plugin-updates/vendor/
+#mkdir -p ${dst_path}/plugin-updates/vendor/
+#for b in ${build[@]}; do
+#    cp ${src_path}${b} ${dst_path}/plugin-updates/vendor/
+#    cp ${src_path}${b} ${debug_path}/plugin-updates/vendor/
+#done
+
+for entry in "${api_path}"/*.json; do
+	echo "Copying ${entry} to ${server}:./${server}/protected-content/api-versions/"
+	scp ${entry} ${server}:./${server}/protected-content/api-versions/
 done
 
 cd ${dst_path}/..
@@ -62,11 +74,13 @@ zip -r ${kit_name}.zip ${plugin_path}
 cd ${debug_path}/..
 cd ${dst_path}/..
 zip -r ${debug_name}.zip ${plugin_path}-debug
-ssh ${server} "cd ./www/protected-content/ ; mkdir -p \"${short_name}\""
-scp ${kit_name}.zip ${server}:./www/protected-content/${short_name}/
-scp ${kit_name}-debug.zip ${server}:./www/protected-content/${short_name}/
-scp ${metadata} ${server}:./www/protected-content/${short_name}/
-ssh ${server} "cd ./www/protected-content/ ; ln -sf \"${short_name}\"/\"${short_name}\"-\"${version}\".zip \"${short_name}\".zip"
+ssh ${server} "cd ./${server}/protected-content/ ; mkdir -p \"${short_name}\""
+scp ${kit_name}.zip ${server}:./${server}/protected-content/${short_name}/
+scp ${kit_name}-debug.zip ${server}:./${server}/protected-content/${short_name}/
+scp ${metadata} ${server}:./${server}/protected-content/${short_name}/
+ssh ${server} "cd ./${server}/protected-content/ ; ln -sf \"${short_name}\"/\"${short_name}\"-\"${version}\".zip \"${short_name}\".zip"
+
+
 rm -rf ${dst_path}
 rm -rf ${debug_path}
 
